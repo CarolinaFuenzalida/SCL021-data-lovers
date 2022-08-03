9402b1faf8dd604a09f772a685c8418c11edc227
CarolinaFuenzalida <carolinapazfuenzalidapoblete@gmail.com>
2022-08-03 13:18:36 -0400

    cambios en create cards y css

--- README.md
+++ README.md
@@ -12,6 +12,33 @@
 * [8. Pistas, tips y lecturas complementarias](#8-pistas-tips-y-lecturas-complementarias)
 * [9. Checklist](#9-checklist)
 
+
+
+# Data Lovers
+
+## Índice 
+
+* [1. About  our page] (#1-About-our-page)
+* [2. Historias de Usuario] (#2-Historias-de-usuario)
+
+
+***
+
+## 1. About Our Page 
+
+Studio Ghibli es un estudio japonés de animación el cuál ha producido y entregado al mundo del cine varios éxitos de taquilla aclamados por la crítica, dentro de sus películas más exitosas se encuentran "El viaje de Chihiro", "El cuento de la Princesa Kaguya", "Mi vecino Totoro", entre otros.
+
+Las historias relatadas en sus obras son bastante variadas, captando así la atención de una amplia gama de usuarios, desde niños a adultos.
+Nuestra misión es llegar a este público y presentarles una página amigable con la cuál puedan interactuar fácilmente 
+
+Nuestra página está dedicada a las personas que están comenzando a adentrarse en el mundo de Ghibli, usuarios que están ansiosos por conocer más sobre las obras producidas en el estudio, incluyendo descripciones, personajes, años de lanzamiento y demás.
+
+En orden de hacer la página más amigable, instalamos una tabla de filtros en donde se puede filtrar por orden alfabético, cronológico, director, productor y puntaje.
+
+
+
+Historia de Usuario 1 
+
 ***
 
 ## 1. Preámbulo
--- src/main.js
+++ src/main.js
@@ -1,72 +1,188 @@
-import data from './data/ghibli/ghibli.js'
+import data from './data/ghibli/ghibli.js';
 import { filterAZ, filterZA, filterMostRecent, filterOldest, filterDirector, filterProducer, filterHighScore, filterLowScore } from './data.js';
 //import { showPopUp, charInfo } from './popup.js';
 //información
-const allFilms = data.films;
-const allCharacters = allFilms.map(f => f.people);
-// const allLocations = allFilms.map(f => f.locations);
-// const allVehicles = allFilms.map(f => f.vehicles);
+let allFilms = data.films;
+let allCharacters = allFilms.map(f => f.people);
+let allLocations = allFilms.map(f => f.locations);
+let allVehicles = allFilms.map(f => f.vehicles);
 const filterOrder = document.getElementById("filterOrder");
-let filmCards = '';
+let filmCards = [];
+console.log(allFilms)
 
 //aplicar filtro A-Z
 filterOrder.addEventListener("change", applyFilterAZ);
 function applyFilterAZ() {
   if (filterOrder.value === "0") {
-    filterAZ(allFilms);
+    let filteredInfo = filterAZ(allFilms);
     createCards(filterAZ);
+    // Crea Info de Pelicula seleccionada
+    document.querySelectorAll(".filmCards").forEach((e, i) => {
+      e.addEventListener("click", () => {
+        let popUpInfo = createPopUp(filteredInfo[i]);
+        document.getElementById("detailHead").innerHTML = "";
+        document.getElementById("detailHead").innerHTML = popUpInfo;
+        let charInfo = createCharacters(filteredInfo.map(f => f.people)[i]);
+        document.getElementById("detailBody").innerHTML = "";
+        document.getElementById("detailBody").innerHTML = charInfo;
+        let locInfo = createLocations(filteredInfo.map(f => f.locations)[i]);
+        document.getElementById("locInfo").innerHTML = "";
+        document.getElementById("locInfo").innerHTML = locInfo;
+        let vehInfo = createVehicles(filteredInfo.map(f => f.vehicles)[i]);
+        document.getElementById("vehInfo").innerHTML = "";
+        document.getElementById("vehInfo").innerHTML = vehInfo;
+        document.getElementById("popUp").showModal();
+      })
+    }
+    )
   }
-}
+};
 //aplicar filtro Z-A
 filterOrder.addEventListener("change", applyFilterZA);
 function applyFilterZA() {
   if (filterOrder.value === "1") {
-    filterZA(allFilms);
+    let filteredInfo = filterZA(allFilms);
     createCards(filterZA);
+    // Crea Info de Pelicula seleccionada
+    document.querySelectorAll(".filmCards").forEach((e, i) => {
+      e.addEventListener("click", () => {
+        let popUpInfo = createPopUp(filteredInfo[i]);
+        document.getElementById("detailHead").innerHTML = "";
+        document.getElementById("detailHead").innerHTML = popUpInfo;
+        let charInfo = createCharacters(filteredInfo.map(f => f.people)[i]);
+        document.getElementById("detailBody").innerHTML = "";
+        document.getElementById("detailBody").innerHTML = charInfo;
+        let locInfo = createLocations(filteredInfo.map(f => f.locations)[i]);
+        document.getElementById("locInfo").innerHTML = "";
+        document.getElementById("locInfo").innerHTML = locInfo;
+        let vehInfo = createVehicles(filteredInfo.map(f => f.vehicles)[i]);
+        document.getElementById("vehInfo").innerHTML = "";
+        document.getElementById("vehInfo").innerHTML = vehInfo;
+        document.getElementById("popUp").showModal();
+      })
+    }
+    )
   }
-}
+};
 //aplicar filtro año de estreno más reciente
 filterOrder.addEventListener("change", applyFilterMostRecent);
 function applyFilterMostRecent() {
   if (filterOrder.value === "2") {
-    filterMostRecent(allFilms);
+    let filteredInfo = filterMostRecent(allFilms);
     createCards(filterMostRecent);
+    // Crea Info de Pelicula seleccionada
+    document.querySelectorAll(".filmCards").forEach((e, i) => {
+      e.addEventListener("click", () => {
+        let popUpInfo = createPopUp(filteredInfo[i]);
+        document.getElementById("detailHead").innerHTML = "";
+        document.getElementById("detailHead").innerHTML = popUpInfo;
+        let charInfo = createCharacters(filteredInfo.map(f => f.people)[i]);
+        document.getElementById("detailBody").innerHTML = "";
+        document.getElementById("detailBody").innerHTML = charInfo;
+        let locInfo = createLocations(filteredInfo.map(f => f.locations)[i]);
+        document.getElementById("locInfo").innerHTML = "";
+        document.getElementById("locInfo").innerHTML = locInfo;
+        let vehInfo = createVehicles(filteredInfo.map(f => f.vehicles)[i]);
+        document.getElementById("vehInfo").innerHTML = "";
+        document.getElementById("vehInfo").innerHTML = vehInfo;
+        document.getElementById("popUp").showModal();
+      })
+    }
+    )
   }
-}
+};
 //aplicar filtro año de estreno más antiguo
 filterOrder.addEventListener("change", applyFilterOldest);
 function applyFilterOldest() {
   if (filterOrder.value === "3") {
-    filterOldest(allFilms);
+    let filteredInfo = filterOldest(allFilms);
     createCards(filterOldest);
+    // Crea Info de Pelicula seleccionada
+    document.querySelectorAll(".filmCards").forEach((e, i) => {
+      e.addEventListener("click", () => {
+        let popUpInfo = createPopUp(filteredInfo[i]);
+        document.getElementById("detailHead").innerHTML = "";
+        document.getElementById("detailHead").innerHTML = popUpInfo;
+        let charInfo = createCharacters(filteredInfo.map(f => f.people)[i]);
+        document.getElementById("detailBody").innerHTML = "";
+        document.getElementById("detailBody").innerHTML = charInfo;
+        let locInfo = createLocations(filteredInfo.map(f => f.locations)[i]);
+        document.getElementById("locInfo").innerHTML = "";
+        document.getElementById("locInfo").innerHTML = locInfo;
+        let vehInfo = createVehicles(filteredInfo.map(f => f.vehicles)[i]);
+        document.getElementById("vehInfo").innerHTML = "";
+        document.getElementById("vehInfo").innerHTML = vehInfo;
+        document.getElementById("popUp").showModal();
+      })
+    }
+    )
   }
-}
+};
 //aplicar filtro puntaje mayor - menor
 filterOrder.addEventListener("change", applyFilterHigh);
 function applyFilterHigh() {
   if (filterOrder.value === "4") {
-    filterHighScore(allFilms);
+    let filteredInfo = filterHighScore(allFilms);
     createCards(filterHighScore);
+    // Crea Info de Pelicula seleccionada
+    document.querySelectorAll(".filmCards").forEach((e, i) => {
+      e.addEventListener("click", () => {
+        let popUpInfo = createPopUp(filteredInfo[i]);
+        document.getElementById("detailHead").innerHTML = "";
+        document.getElementById("detailHead").innerHTML = popUpInfo;
+        let charInfo = createCharacters(filteredInfo.map(f => f.people)[i]);
+        document.getElementById("detailBody").innerHTML = "";
+        document.getElementById("detailBody").innerHTML = charInfo;
+        // Probando si crea Loc
+        let locInfo = createLocations(filteredInfo.map(f => f.locations)[i]);
+        document.getElementById("locInfo").innerHTML = "";
+        document.getElementById("locInfo").innerHTML = locInfo;
+        let vehInfo = createVehicles(filteredInfo.map(f => f.vehicles)[i]);
+        document.getElementById("vehInfo").innerHTML = "";
+        document.getElementById("vehInfo").innerHTML = vehInfo;
+        document.getElementById("popUp").showModal();
+      })
+    }
+    )
   }
-}
+};
 //aplicar filtro puntaje menor - mayor
 filterOrder.addEventListener("change", applyFilterLow);
 function applyFilterLow() {
   if (filterOrder.value === "5") {
-    filterLowScore(allFilms);
+    let filteredInfo = filterLowScore(allFilms);
     createCards(filterLowScore);
+    // Crea Info de Pelicula seleccionada
+    document.querySelectorAll(".filmCards").forEach((e, i) => {
+      e.addEventListener("click", () => {
+        let popUpInfo = createPopUp(filteredInfo[i]);
+        document.getElementById("detailHead").innerHTML = "";
+        document.getElementById("detailHead").innerHTML = popUpInfo;
+        let charInfo = createCharacters(filteredInfo.map(f => f.people)[i]);
+        document.getElementById("detailBody").innerHTML = "";
+        document.getElementById("detailBody").innerHTML = charInfo;
+        let locInfo = createLocations(filteredInfo.map(f => f.locations)[i]);
+        document.getElementById("locInfo").innerHTML = "";
+        document.getElementById("locInfo").innerHTML = locInfo;
+        let vehInfo = createVehicles(filteredInfo.map(f => f.vehicles)[i]);
+        document.getElementById("vehInfo").innerHTML = "";
+        document.getElementById("vehInfo").innerHTML = vehInfo;
+        document.getElementById("popUp").showModal();
+      })
+    }
+    )
   }
-}
+};
 //Filtrar por director
 //filterBtn.addEventListener("click", applyFilterDirector);
 document.getElementById("filterByDirector").addEventListener("change", applyFilterDirector)
 function applyFilterDirector() {
   //let filterInput = document.getElementById("filterByDirector")
   let selectedDirector = document.getElementById("filterByDirector").value
+  let filteredInfo = filterDirector(allFilms, selectedDirector)
   if (selectedDirector === "all") {
     createCards()
   } else {
-    let filteredInfo = filterDirector(allFilms, selectedDirector)
     document.getElementById("filmsCont").innerHTML = '';
     for (let i = 0; i < filteredInfo.length; i++) {
       filmCards = `<div class="filmCards" > <div class="filmPosters"> <img src="${filteredInfo[i].poster}" alt="Poster de la película"> </div>
@@ -75,18 +191,38 @@ function applyFilterDirector() {
       document.getElementById("filmsCont").innerHTML += filmCards;
     }
   }
+  // Crea Info de Pelicula seleccionada
+  document.querySelectorAll(".filmCards").forEach((e, i) => {
+    e.addEventListener("click", () => {
+      let popUpInfo = createPopUp(filteredInfo[i]);
+      document.getElementById("detailHead").innerHTML = "";
+      document.getElementById("detailHead").innerHTML = popUpInfo;
+      let charInfo = createCharacters(filteredInfo.map(f => f.people)[i]);
+      document.getElementById("detailBody").innerHTML = "";
+      document.getElementById("detailBody").innerHTML = charInfo;
+      let locInfo = createLocations(filteredInfo.map(f => f.locations)[i]);
+      document.getElementById("locInfo").innerHTML = "";
+      document.getElementById("locInfo").innerHTML = locInfo;
+      let vehInfo = createVehicles(filteredInfo.map(f => f.vehicles)[i]);
+      document.getElementById("vehInfo").innerHTML = "";
+      document.getElementById("vehInfo").innerHTML = vehInfo;
+      document.getElementById("popUp").showModal();
+    })
+  }
+  )
 }
 
 //Filtrar por productor
 //filterBtn.addEventListener("click", applyFilterProducer);
 document.getElementById("filterByProducer").addEventListener("change", applyFilterProducer)
 function applyFilterProducer() {
+  let filteredInfoProducer = [];
   // let filterInput = document.getElementById("filterByProducer")
   let selectedProducer = document.getElementById("filterByProducer").value
   if (selectedProducer === "all") {
     createCards()
   } else {
-    let filteredInfoProducer = filterProducer(allFilms, selectedProducer)
+    filteredInfoProducer = filterProducer(allFilms, selectedProducer)
     document.getElementById("filmsCont").innerHTML = '';
     for (let i = 0; i < filteredInfoProducer.length; i++) {
       filmCards = `<div class="filmCards" > <div class="filmPosters"> <img src="${filteredInfoProducer[i].poster}" alt="Poster de la película"> </div>
@@ -95,7 +231,26 @@ function applyFilterProducer() {
       document.getElementById("filmsCont").innerHTML += filmCards;
     }
   }
-}
+  // Crea Info de Pelicula seleccionada
+  document.querySelectorAll(".filmCards").forEach((e, i) => {
+    e.addEventListener("click", () => {
+      let popUpInfo = createPopUp(filteredInfoProducer[i]);
+      document.getElementById("detailHead").innerHTML = "";
+      document.getElementById("detailHead").innerHTML = popUpInfo;
+      let charInfo = createCharacters(filteredInfoProducer.map(f => f.people)[i]);
+      document.getElementById("detailBody").innerHTML = "";
+      document.getElementById("detailBody").innerHTML = charInfo;
+      let locInfo = createLocations(filteredInfoProducer.map(f => f.locations)[i]);
+      document.getElementById("locInfo").innerHTML = "";
+      document.getElementById("locInfo").innerHTML = locInfo;
+      let vehInfo = createVehicles(filteredInfoProducer.map(f => f.vehicles)[i]);
+      document.getElementById("vehInfo").innerHTML = "";
+      document.getElementById("vehInfo").innerHTML = vehInfo;
+      document.getElementById("popUp").showModal();
+    })
+  }
+  )
+};
 
 
 //iterar datos y desplayar la información (posters y títulos) en HTML
@@ -119,28 +274,37 @@ function createCards() {
     <div><p class="filmTitles">${allFilms[i].title}</p></div> 
   </div>`
     document.getElementById("filmsCont").innerHTML += filmCards;
-  } 
+  }
 
   // Crea Info de Pelicula seleccionada
-   document.querySelectorAll(".filmCards").forEach ((e,i) => {
-    e.addEventListener("click" , () => {
-     let popUpInfo = createPopUp(allFilms[i]);
-      document.getElementById("detailHead").innerHTML ="";
+
+  document.querySelectorAll(".filmCards").forEach((e, i) => {
+    e.addEventListener("click", () => {
+      let popUpInfo = createPopUp(allFilms[i]);
+      document.getElementById("detailHead").innerHTML = "";
       document.getElementById("detailHead").innerHTML = popUpInfo;
-     
-      //añadir for each
       let charInfo = createCharacters(allCharacters[i]);
-       document.getElementById("detailBody").innerHTML= "";
-     document.getElementById("detailBody").innerHTML = charInfo;
-  
+      document.getElementById("detailBody").innerHTML = "";
+      document.getElementById("detailBody").innerHTML = charInfo;
+      let locInfo = createLocations(allLocations[i]);
+      document.getElementById("locInfo").innerHTML = "";
+      document.getElementById("locInfo").innerHTML = locInfo;
+      let vehInfo = createVehicles(allVehicles[i]);
+      document.getElementById("vehInfo").innerHTML = "";
+      document.getElementById("vehInfo").innerHTML = vehInfo;
+      document.getElementById("popUp").showModal();
     })
   }
-)}
+  )
+};
+
+
 //Muestra todas las peliculas desde la barra de filtros 
 const showAllCards = document.getElementById("showAllCards")
 showAllCards.addEventListener("click", createCards);
 
 
+//Crea los Pop Up 
 function createPopUp(allFilms) {
   return `<div class="filmInfoPosters"> 
       <img src="${allFilms.poster}" alt="Movie poster">    
@@ -157,10 +321,11 @@ function createPopUp(allFilms) {
     </div>`
 }
 
+
 function createCharacters(allCharacters) {
   let htmlChar = "";
   for (let i = 0; i < allCharacters.length; i++) {
-  htmlChar = htmlChar + `<div class="charInfo">
+    htmlChar = htmlChar + `<div class="charInfo">
       <div class="charImg">
         <img src="${allCharacters[i].img}" alt="Character picture">
       </div>
@@ -175,11 +340,54 @@ function createCharacters(allCharacters) {
         </ul>
       </div>
       </div>`
- }
+  }
   return htmlChar
 }
 
+function createLocations(allLocations) {
+  let htmlLoc = "";
+  for (let i = 0; i < allLocations.length; i++) {
+    htmlLoc = htmlLoc + `<div class="locInfo">
+      <div class="locImg">
+        <img  src="${allLocations[i].img}" alt="Location picture">
+      </div>
+      <div class="locText">
+        <ul>
+          <li>Name: ${allLocations[i].name}</li>
+          <li>Climate: ${allLocations[i].climate}</li>
+          <li>Terrain: ${allLocations[i].terrain}</li>
+          <li>Surface Water: ${allLocations[i].surface_water}</li>
+        </ul>
+      </div>
+    </div>`
+  }
+  return htmlLoc
+};
 
+function createVehicles(allVehicles) {
+  let htmlVeh = "";
+  for (let i = 0; i < allVehicles.length; i++) {
+    htmlVeh = htmlVeh + ` <div class="vehInfo">
+      <div class="vehImg">
+        <img src="${allVehicles[i].img}" alt="Vehicle picture">
+      </div>
+      <div class="vehText">
+        <ul>
+         <li>Name: ${allVehicles[i].name}</li>
+          <li>Description: ${allVehicles[i].description}</li>
+          <li>Type: ${allVehicles[i].vehicle_class}</li>
+          <li>Length: ${allVehicles[i].length}</li>
+        </ul>
+      </div>`
+  }
+  return htmlVeh
+};
 
+// btnCharacters.addEventListener("click", createCharacters);
+// btnVehicles.addEventenListener("click", createLocations);
 
-
+//Cierra el Pop Up
+document.getElementById("btnClosePopUp").addEventListener("click", closePopUp)
+function closePopUp() {
+  document.getElementById("popUp").close();
+}
